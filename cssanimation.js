2d1c1e41be382da93790dc8b3d1a62177f098ea3
Tadas1985 <tadas1985@gmail.com>
2018-09-26 21:48:11 +0200

    Update cssanimation.js

--- cssanimation.js
+++ cssanimation.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
 	$('.box').on('click', function () {
-		var animations = [ 'crawl', 'dangle', 'jump', 'stretch', 'flip' ];
+		var animations = [ 'crawl', 'dangle', 'jump', 'stretch', 'flip','slalom','fade','grow','running','turn' ];
 		var box = this;
-		var animation = animations[Math.floor(Math.random() * 5)];
+		var animation = animations[Math.floor(Math.random() * 10)];
 
 		$(box).addClass(animation);
 
